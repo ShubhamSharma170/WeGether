4ce64c194852710451b2706d156b276545319012
Shubham Sharma <170sharmashubham@gmail.com>
2025-11-14 13:16:03 +0530

    UI Compelte in phone number page

--- lib/pages/settings/edit_profile_pages/phone.dart
+++ lib/pages/settings/edit_profile_pages/phone.dart
@@ -1,16 +1,35 @@
+import 'package:chat_app/components/button.dart';
+import 'package:chat_app/components/textfield.dart';
 import 'package:flutter/material.dart';
 
-class PhonePage extends StatelessWidget {
+class PhonePage extends StatefulWidget {
   const PhonePage({super.key});
+
+  @override
+  State<PhonePage> createState() => _PhonePageState();
+}
+
+class _PhonePageState extends State<PhonePage> {
+  final TextEditingController controller = TextEditingController();
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Edit Phone Number"),
-      ),
-      body: const Center(
-        child: Text("Phone Number Edit Page"),
+      appBar: AppBar(title: const Text("Edit Phone Number")),
+      body: Center(
+        child: Column(
+          children: [
+            MyTextField(hintText: "Enter Phone Number", controller: controller),
+            SizedBox(height: 20),
+            MyButton(buttonName: "Save", onTap: () async {}),
+          ],
+        ),
       ),
     );
   }
