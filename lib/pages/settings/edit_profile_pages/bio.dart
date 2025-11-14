34a20f1dc5cc1a2c518f0f4a1fa08b2489e7a33b
Shubham Sharma <170sharmashubham@gmail.com>
2025-11-14 13:03:46 +0530

    UI Compelte

--- lib/pages/settings/edit_profile_pages/bio.dart
+++ lib/pages/settings/edit_profile_pages/bio.dart
@@ -1,16 +1,31 @@
+import 'package:chat_app/components/button.dart';
+import 'package:chat_app/components/textfield.dart';
 import 'package:flutter/material.dart';
 
-class BioPage extends StatelessWidget {
+class BioPage extends StatefulWidget {
   const BioPage({super.key});
 
   @override
+  State<BioPage> createState() => _BioPageState();
+}
+
+class _BioPageState extends State<BioPage> {
+  TextEditingController controller = TextEditingController();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Edit Bio"),
-      ),
-      body: const Center(
-        child: Text("Bio Edit Page"),
+      appBar: AppBar(title: const Text("Edit Bio")),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            children: [
+              MyTextField(hintText: "Enter Bio", controller: controller),
+              SizedBox(height: 20),
+              MyButton(buttonName: "Save", onTap: () async {}),
+            ],
+          ),
+        ),
       ),
     );
   }
